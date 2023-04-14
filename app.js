30650de396db4b6f608ae9ff84e75043f0ee5498
pnoikid <rob.pena83@gmail.com>
2023-04-13 20:58:39 -0400

    changed background

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const form = document.querySelector("#form")
 const submitButton = document.querySelector("#submit")
-const scriptURL = 'https://script.google.com/macros/s/1XTaVdkdAnzhAHoLOA-x0K4svtJOB9RUnp2OgudFCNoEE4Lo8muDKOgSO/exec'
+const scriptURL = "https://httpbin.org/post";
 
 form.addEventListener('submit', e => {
   submitButton.disabled = true
